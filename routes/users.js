a5366f12145b0be3f359be27cad14018ff652434
Jonathan Lee <j47lee@gmail.com>
2015-08-17 16:41:06 -0700

    added users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var usersController = require('../controllers/usersController');
+
+//GET ALL
+router.get('/users', usersController.getIndex);
+//GET NEW FORM
+router.get('/users/new', usersController.new);
+//CREATE AFTER FORM SUBMISSION
+router.post('/users', usersController.create);
+//GET ONE
+router.get('/users/:id', usersController.getOne);
+//GET EDIT FORM
+router.get('/users/:id/edit', usersController.edit);
+//UPDATE AFTER FORM SUBMISSION
+router.put('/users/:id/', usersController.update);
+//DELETE ONE
+router.delete('/users/:id', usersController.destroy);
 
 module.exports = router;
