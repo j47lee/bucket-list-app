213e9ebccbf7a1e8e4bbcbed9047e9f8cae21ab2
Jonathan Lee <j47lee@gmail.com>
2015-08-17 17:58:21 -0700

    added use routes in app.js

--- app.js
+++ app.js
@@ -35,8 +35,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//USE ROUTES
+app.use(routes);
+// app.use('/', routes);  //to be removed
+// app.use('/users', users);  //to be removed
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,6 +70,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
