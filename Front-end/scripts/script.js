e760a303e455ba8aa157b22bec23906c6b641ece
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-20 09:17:08 -0700

    set up ajax sign up form

--- Front-end/scripts/script.js
+++ Front-end/scripts/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){//Load JQuery on Ready
-  
+var loginForm    = $('#login-form');
+var loginButton  = $('#login-btn');
+var signUpForm   = $('#sign-up-form');
+var signUpButton = $('#sign-up-btn');
+
+
 // USER LOG IN REQUEST
 // ==========================================================
-var loginForm   = $('#login-form');
-var loginButton = $('#login-btn');
 
 loginForm.on('submit',function(evt) {
   evt.preventDefault();
@@ -26,7 +29,35 @@ loginForm.on('submit',function(evt) {
       console.log(data);
     }
   });//END POST
-});
+});//END LOG IN REQUEST
+
+// NEW USER SIGN UP REQUEST
+// ==========================================================
+signUpForm.on('submit',function (evt) {
+  evt.preventDefault();
+  console.log(evt.target.email.value + evt.target.password.value);
+
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:8080/sign-up",
+    dataType: 'json',
+    contentType: 'application/json; charset=UTF-8',
+    data : JSON.stringify({user: { name: evt.target.name.value,
+                                   email: evt.target.email.value ,
+                                   password: evt.target.password.value}}),
+    success : function (returnedData) {
+      console.log("success");
+      console.dir(returnedData);
+      window.location = "users";
+    },
+    error: function (error, data) {
+      console.log('error handler');
+      console.log(error);
+      console.log(data);
+    }
+  });//END POST REQUEST
+})//END SIGN UP REQUEST
+
 
 
 });//End Document Ready
