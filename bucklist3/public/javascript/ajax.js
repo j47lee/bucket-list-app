6d47959e8a00bae6e1b9918cf1e27fc4636b3313
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-21 00:20:47 -0700

    styled tweets

--- bucklist3/public/javascript/ajax.js
+++ bucklist3/public/javascript/ajax.js
@@ -27,14 +27,10 @@ function searchTweets(search_term) {
       method: 'get',
       url: 'http://localhost:8080/twitter',
       success: function(data) {
-        // console.dir(data.users[0]);
-        // for(item in data['results']){
-        //   $('#tweets').append("<div>" + data['results'][item]['text'] + "</div>")
-        // }
         for (var i = 0; i < 5; i++) {
           console.dir(data.users[i]);
           $('.tweet-results').append(
-            "<div class='tweets'>" +   '<a href="' + data.users[i].url + '">' + '<img src="' + data.users[i].imgSmall +'" />'  + data.users[i].name  + "</div>"
+            "<div class='tweets'>" +   '<a href="' + data.users[i].url + '">' + '<img src="' + data.users[i].imgSmall +'" />'  + "<p>" + data.users[i].name + "</p>" + "</div>"
           )
         }
       },
